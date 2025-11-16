4e40e2014d052ff6a7734d063bde42930b810f9e
koh38purdue <koh38@purdue.edu>
2025-11-16 17:06:11 -0500

    temp sensor updates

--- 362_Group_Project/src/temp.c
+++ 362_Group_Project/src/temp.c
@@ -4,6 +4,7 @@
 #include "hardware/irq.h"
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
+#include "math.h"
 
 #define I2C_SDA 28
 #define I2C_SCL 29
@@ -12,9 +13,15 @@
 
 #define SHT30_ADDR 0x44 //addr pin connected to logic low
 
-// 0x2024 to start periodic measurements @ medium repeatability and 0.5mps
+// 0x2126 to start periodic measurements @ medium repeatability and 1mps
 // 0xE000 to fetch data
 // 0x3093 to stop periodic measurement
+
+void init_i2c();
+void start_measurement();
+void fetch_data(uint8_t *data);
+void stop_measurement();
+
 
 
 int main() {
@@ -22,10 +29,21 @@ int main() {
     init_i2c();
     start_measurement();
 
-    uint8_t data[2];
-    fetch_data(data);
+    uint8_t data[4];
+
+    uint16_t temp_b;
+    uint16_t humid_b;
+
     while(1) {
-        printf("temp: %d, hum: %d\n", data[0], data[1]);
+        fetch_data(data);
+
+        temp_b = (data[0] << 8) | data[1];
+        humid_b = (data[2] << 8) | data[3];
+
+        float temp = 315.0 * ((float) temp_b) / (pow(2, 16) - 1) - 49;
+        float humid = 100.0 * ((float) humid_b) / (pow(2, 16) - 1);
+
+        printf("temp: %.1f°F, hum: %.1f%%\n", temp, humid);
         sleep_ms(2000);
     }
 }
@@ -39,18 +57,37 @@ void init_i2c() {
 
 void start_measurement() {
     uint8_t control[2];
-    control[0] = 0x20;
-    control[1] = 0x24;
+    control[0] = 0x21;
+    control[1] = 0x26;
 
     i2c_write_blocking(I2C_inst, SHT30_ADDR, control, 2, false);
 }
 
-void fetch_data(uint8_t *data) {
-    uint8_t control[2];
-    control[0] = 0xE0;
-    control[1] = 0x00;
+// void fetch_data(uint8_t *data) {
+//     uint8_t control[2];
+//     control[0] = 0xE0;
+//     control[1] = 0x00;
 
-    i2c_read_blocking(I2C_inst, control, data, 2, false);
+//     i2c_write_blocking(I2C_inst, SHT30_ADDR, control, 2, false);
+
+//     i2c_read_blocking(I2C_inst, SHT30_ADDR, data, 4, false);
+// }
+
+void fetch_data(uint8_t *data) {
+    uint8_t fetch_cmd[2] = {0xE0, 0x00};  // Fetch data command
+    
+    // Send fetch command
+    i2c_write_blocking(I2C_inst, SHT30_ADDR, fetch_cmd, 2, true);  // true = repeated start
+    
+    // Read 6 bytes: temp_msb, temp_lsb, temp_crc, humid_msb, humid_lsb, humid_crc
+    uint8_t buffer[6];
+    i2c_read_blocking(I2C_inst, SHT30_ADDR, buffer, 6, false);
+    
+    // Copy temperature and humidity data (skipping CRC bytes for now)
+    data[0] = buffer[0];  // temp MSB
+    data[1] = buffer[1];  // temp LSB
+    data[2] = buffer[3];  // humid MSB
+    data[3] = buffer[4];  // humid LSB
 }
 
 void stop_measurement() {
